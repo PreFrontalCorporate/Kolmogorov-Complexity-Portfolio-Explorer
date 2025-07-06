490849877d8310ea5a953ecc1f2db901e78527af
Prefrontal Corporate <prefrontalcorporate@gmail.com>
2025-07-05 20:31:03 -0700

    COMMIT
    Update optimize.py

--- api/optimize.py
+++ api/optimize.py
@@ -1,15 +1,14 @@
+# kolmogorov_app/api/optimize.py
+
+from flask import Blueprint, request, jsonify
 import numpy as np
-from flask import Flask, request, jsonify
 from scipy.optimize import dual_annealing
 from pyswarm import pso
 
 from api.utils import compute_VaR, compute_CVaR
 
-app = Flask(__name__)
+kolmogorov_bp = Blueprint("kolmogorov", __name__)
 
-# -----------------------------------
-# Objective Function
-# -----------------------------------
 def objective_function(weights, *args):
     cov_matrix, expected_returns, lambda_0, lambda_1 = args
     risk = np.dot(weights.T, np.dot(cov_matrix, weights))
@@ -18,56 +17,18 @@ def objective_function(weights, *args):
     magnitude = lambda_1 * np.sum(np.abs(weights))
     return risk - return_ + sparsity + magnitude
 
-# -----------------------------------
-# Simulated Annealing Optimizer
-# -----------------------------------
 def optimize_portfolio_sa(X, lambda_0, lambda_1):
     n_assets = X.shape[1]
     lb = np.zeros(n_assets)
     ub = np.ones(n_assets) * 0.1
-
     cov_matrix = np.cov(X.T)
     expected_returns = np.mean(X, axis=0)
-
-    result_sa = dual_annealing(
-        objective_function,
-        bounds=list(zip(lb, ub)),
-        maxiter=2000,
-        maxfun=2000,
-        args=(cov_matrix, expected_returns, lambda_0, lambda_1)
-    )
+    result_sa = dual_annealing(objective_function, bounds=list(zip(lb, ub)), maxiter=2000, maxfun=2000, args=(cov_matrix, expected_returns, lambda_0, lambda_1))
     return result_sa.x
 
-# -----------------------------------
-# Particle Swarm Optimizer (Optional)
-# -----------------------------------
-def optimize_portfolio_pso(X, lambda_0, lambda_1):
-    n_assets = X.shape[1]
-    lb = np.zeros(n_assets)
-    ub = np.ones(n_assets) * 0.1
-
-    cov_matrix = np.cov(X.T)
-    expected_returns = np.mean(X, axis=0)
-
-    optimal_weights, _ = pso(
-        objective_function,
-        lb,
-        ub,
-        maxiter=2000,
-        swarmsize=80,
-        c1=2,
-        c2=2,
-        args=(cov_matrix, expected_returns, lambda_0, lambda_1)
-    )
-    return optimal_weights
-
-# -----------------------------------
-# API Endpoint
-# -----------------------------------
-@app.route('/optimize', methods=['POST'])
+@kolmogorov_bp.route('/optimize', methods=['POST'])
 def optimize_portfolio():
     data = request.json
-
     returns_data = np.array(data['returns_data'])
     lambda_0 = data['lambda_0']
     lambda_1 = data['lambda_1']
@@ -84,9 +45,3 @@ def optimize_portfolio():
             'CVaR': cvar
         }
     })
-
-# -----------------------------------
-# Run
-# -----------------------------------
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
